05892cabfa47cb6f30b7f5d49562a0f94f4d6df0
Anupras Narayan <narayananupras99@gmail.com>
2026-06-05 12:04:16 +0530

    varidatra

---
+++ assets/videos/varidata.js
@@ -0,0 +1,42 @@
+//string examle
+
+let  a = "Rahul Jain";
+      b = "Susmita Singh";
+      c = "Neha Singh";
+      d  = "Ashistosh Rana"
+
+console.log(a)
+console.log(typeof(a))
+console.log(b)
+console.log(typeof(b))
+console.log(c)
+console.log(typeof(c))
+console.log(d)
+console.log(typeof(d))
+
+//number
+let personphoneno = 3657846752;
+
+console.log(personphoneno)
+console.log(typeof(personphoneno))
+
+//boolean
+let right = "true"
+    wrong = "False"
+
+    console.log(right)
+    console.log(typeof(right))
+    console.log(false)
+    console.log(typeof(false))
+
+//null
+let middleman = null
+
+console.log(middleman)
+console.log(typeof(middleman))
+
+//undefined
+let student = undefined;
+
+console.log(student)
+console.log(typeof(student))

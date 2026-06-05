0402bf19f63812c7bbc55b93c89d309f90fd94c7
Anupras Narayan <narayananupras99@gmail.com>
2026-06-05 14:08:35 +0530

    hlo

---
+++ assets/html-js/html-js/stuinfopro.js
@@ -0,0 +1,9 @@
+//Student Information Program
+
+let name="Anshu Mehta";
+Course = "B.Tech";
+City = "Lucknow"
+
+console.log(Name)
+console.log(Course)
+console.log(City)
---
+++ task/arepara.js
@@ -0,0 +1,49 @@
+//rectangle area and paremeter
+
+const l = 6;
+const b = 4;
+
+console.log(l*b)
+console.log(2*l+b)
+
+//square area and paremeter
+
+const a = 17;
+
+console.log(4*a)
+console.log(a*a)
+
+//triangle area and paremeter
+ 
+const e = 12;
+const x= 10;
+const c= 6;
+const h = 5;
+
+console.log(e+x+c)
+console.log(1/2*c*h)
+
+//Rombus area and paremeter
+const w = 12;
+ const d1 = 2;
+ const d2 = 3;
+
+ console.log(4*w)
+ console.log(1/2*d1*d2)
+
+ //parallelogram area and paremeter
+const t=7;
+  const y=5;
+  const u=6;
+
+ console.log(2*t*y)
+  console.log(1/2*u*y)
+
+  //trapezium area and parameter
+  let j =3;
+  let k=8;
+  let z=9;
+  let m=5;
+
+  console.log(j+k+z+m)
+console.log(1/2*k+z*m)
